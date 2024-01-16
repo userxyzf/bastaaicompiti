f834e7171d2a3ee933048561930f6f336fbaf726
userxyzf <156821450+userxyzf@users.noreply.github.com>
2024-01-16 16:08:22 +0100

    Add files via upload

---
+++ js/searchindex.js
@@ -0,0 +1,18 @@
+// search index for Quick 'n Easy Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("index.html", "Untitled Page", "untitled page stop ai compiti per casa la scuola intelligente ma non si applica quante volte ti sarai detto quot oh che bello egrave il momento di tornare vedo ora ograve poi vai sul tuo registro elettronico ritrovi cos igrave tante pagine da studiare oppure tanti esercizi fanno dire ne posso pi ugrave questi voglio uscire nbsp lo sbaglio della proprio questo dovrebber essere piacevoli uno studente dovrebbe apprezzare ci impara fa sopra libri se odia studia giusto prendere un numero in pagella cosa servono? sono li fai professori daranno colpa te dicendoti sei svogliato ormai neanche orale viene fatto memoria solo avere voto ricordato nel corso vita ed scritti vengono copiati internet tramite servizi intelligenza artificiale come chatgpt bing google bard piacevole dei senza diventerebbe anche ascoltare essendo hai pensare devi fare molte ragazzi ascoltano via dell ansia gli spetta inoltre nati punizione molti stressati nervosi pure tristi voti verifiche sarebbe sicuramente molto rilassante bella quello verrebbe presa con occhio diverso parlare delle bocciature sentono costretti dover materie cui interessa minimamente esempio storia arte disegno geometrico mito epica ecc lasciamo spazio nostri hanno neanch tempo dormire alziamo dito diciamo firma subito petizione mettiamo su parola fine questa dittatura https www change org aboliamo casa?signed true ", "");
+   return this;
+}
